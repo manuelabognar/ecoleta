ba436651edf25f1d7cf8ab4fa1e1a5a169eee8fd
manuelabognar <manuela.camara@usp.br>
2020-06-04 18:59:06 -0300

    add imagem (fixa) no ponto de coleta

--- backend/src/controllers/PointsController.ts
+++ backend/src/controllers/PointsController.ts
@@ -58,7 +58,7 @@ class PointsController {
     const trx = await knex.transaction();
 
     const point = {
-      image: 'image-fake',
+      image: 'https://images.unsplash.com/photo-1576940255918-156203c7e369?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
       name,
       email,
       whatsapp,
